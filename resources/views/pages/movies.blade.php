3805c547308f045f7bfde0250bc760e0f093d5b9
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-18 01:43:29 +0100

    style

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -7,10 +7,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row row-cols-md-4">
+    <div class="row row-cols-md-4 g-2">
         @forelse ($movies as $movie)
         <div class="col">
-        <div class="card">
+        <div class="card h-100">
             <img src="{{$movie->image}}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">{{$movie->title}}</h5>
