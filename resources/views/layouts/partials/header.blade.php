af2cdeb887819fde20521d48636a2de654f3da1b
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-18 18:31:33 +0100

    style

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="{{route('home')}}">Navbar</a>
+          <div class="logo"><a class="navbar-brand" href="{{route('home')}}"><img src="{{ Vite::asset('resources/img/logo.png') }}" alt="" class="img-fluid"></a></div>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
