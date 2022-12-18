4a218155442837663fc38d7ca1e5b653f8d7e610
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-18 01:52:41 +0100

    style effect scale img

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -10,8 +10,11 @@
     <div class="row row-cols-md-4 g-2">
         @forelse ($movies as $movie)
         <div class="col">
-        <div class="card h-100">
-            <img src="{{$movie->image}}" class="card-img-top" alt="...">
+        <div class="card h-100 overflow-hidden">
+            <div class="overflow-hidden">
+            
+                <img src="{{$movie->image}}" class="card-img-top" alt="...">
+        </div>
             <div class="card-body">
               <h5 class="card-title">{{$movie->title}}</h5>
               <p class="card-text">{{$movie->original_title}}</p>
